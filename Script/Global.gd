3e72f74f8a1b01d9a0f7b1354c8827d1ab724c2e
AlphaBeta <70358251+AlphaBeta1906@users.noreply.github.com>
2020-12-12 17:06:43 +0700

    Add files via upload
    make sure the debug console does'nt print anything excepct highscore data

--- Script/Global.gd
+++ Script/Global.gd
@@ -2,9 +2,7 @@ extends Node
 var health  = 50
 var Score = 0
 var save = File.new()
-var save2 =  File.new()
 var path = "Highscore.dat"
-#var path2 = "User://test.text"
 var data = {"Highscore" : 0}
 var highscore = 0
 
@@ -22,21 +20,13 @@ func Reset():
 
 func create_save():
 	save.open(path, File.WRITE)
-	print(save.get_path())
 	save.store_var(data)
 	save.close()
-	print("save created")
 
 func Load():
 	save.open(path,File.READ)
 	data = save.get_var()
 	highscore = data["Highscore"]
 	save.close()
-"""
-func create_save2():
-	save2.open("C://Users/Salman-pc/AppData/Local/Space striker/test.text", File.WRITE)
-	print(save2.get_path())
-	save2.store_var(data)
-	save2.close()
-	print("save2 created")"""
+
 
